092304dca79bbb89d36ba25b3f3786b4139bdbd0
danielgino <danielgino3@Gmail.com>
2025-05-04 14:12:19 +0300

    removed dotenv loading for Railway compatibility

--- src/main/java/org/example/apimywebsite/configuration/WebConfig.java
+++ src/main/java/org/example/apimywebsite/configuration/WebConfig.java
@@ -13,14 +13,23 @@ public class WebConfig implements WebMvcConfigurer {
     public JwtUtil jwtUtil() {
         return new JwtUtil();
     }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                     .allowedOrigins("http://localhost:3000",
-                             "https://faceboard-frontend.vercel.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-
+                .allowedOrigins("http://localhost:3000", "https://faceboard-frontend.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                     .allowedOrigins("http://localhost:3000",
+//                             "https://faceboard-frontend.vercel.app")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+//
+//    }
 }
 
 
