46d0f50eafa9667dda17954b1076d20399a03f6e
danielgino <danielgino3@Gmail.com>
2025-05-04 12:40:24 +0300

    removed dotenv loading for Railway compatibility

--- src/main/java/org/example/apimywebsite/ApiMyWebsiteApplication.java
+++ src/main/java/org/example/apimywebsite/ApiMyWebsiteApplication.java
@@ -12,14 +12,14 @@ public class ApiMyWebsiteApplication {
 
     public static void main(String[] args) {
 
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("CLOUDINARY_NAME", dotenv.get("CLOUDINARY_NAME"));
-        System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
-        System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+//        Dotenv dotenv = Dotenv.load();
+//        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+//        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+//        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+//        System.setProperty("CLOUDINARY_NAME", dotenv.get("CLOUDINARY_NAME"));
+//        System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
+//        System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
+//        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
 
         SpringApplication.run(ApiMyWebsiteApplication.class, args);
     }
