686df4fc23abadb8dbd8cd26db4ec4715c8a5828
danielgino <danielgino3@Gmail.com>
2025-05-05 23:49:38 +0300

    dotenv commands remove

--- src/main/java/org/example/apimywebsite/repository/StoryRepository.java
+++ src/main/java/org/example/apimywebsite/repository/StoryRepository.java
@@ -3,6 +3,8 @@ package org.example.apimywebsite.repository;
 
 import org.example.apimywebsite.api.model.Story;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,7 +12,12 @@ import java.util.List;
 @Repository
 public interface StoryRepository extends JpaRepository<Story, Long> {
 
-    List<Story> findByUserIdInAndExpiresAtAfter(List<Integer> userIds, LocalDateTime now);
+//    List<Story> findByUserIdInAndExpiresAtAfter(List<Integer> userIds, LocalDateTime now);
+    @Query("SELECT s FROM Story s WHERE s.user.id IN :userIds AND s.expiresAt > :now")
+    List<Story> findByUserIdInAndExpiresAtAfter(@Param("userIds") List<Integer> userIds,
+                                                @Param("now") LocalDateTime now);
 
-    List<Story> findByExpiresAtBefore(LocalDateTime now);
+//    List<Story> findByExpiresAtBefore(LocalDateTime now);
+@Query("SELECT s FROM Story s WHERE s.expiresAt < :now")
+List<Story> findByExpiresAtBefore(@Param("now") LocalDateTime now);
 }
