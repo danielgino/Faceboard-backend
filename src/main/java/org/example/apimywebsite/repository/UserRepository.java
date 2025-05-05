a736dea461a202bbf022280ef7921de81ce74d2a
danielgino <danielgino3@Gmail.com>
2025-05-05 23:49:38 +0300

    dotenv commands remove

--- src/main/java/org/example/apimywebsite/repository/UserRepository.java
+++ src/main/java/org/example/apimywebsite/repository/UserRepository.java
@@ -3,12 +3,18 @@ package org.example.apimywebsite.repository;
 
 import org.example.apimywebsite.api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT u FROM User u WHERE u.email = :email")
     User findByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.userName = :userName")
     List<User> findByNameContaining(String name);
+    @Query("SELECT u FROM User u WHERE u.name LIKE %:name%")
+
     User findByUserName(String userName);
 
 
