e68f1fd4f0ada624f2a202500a6cfea3a2af1561
danielgino <danielgino3@Gmail.com>
2025-05-05 22:56:15 +0300

    dotenv commands remove

--- src/main/java/org/example/apimywebsite/service/CloudinaryService.java
+++ src/main/java/org/example/apimywebsite/service/CloudinaryService.java
@@ -2,6 +2,7 @@ package org.example.apimywebsite.service;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -33,24 +34,36 @@ public class CloudinaryService {
 //}
 
 
-    public CloudinaryService() {
-        String cloudName = System.getenv("CLOUDINARY_NAME");
-        String apiKey = System.getenv("CLOUDINARY_API_KEY");
-        String apiSecret = System.getenv("CLOUDINARY_API_SECRET");
-
-        if (cloudName == null || apiKey == null || apiSecret == null) {
-            throw new RuntimeException("❌ One or more Cloudinary env vars are missing!");
-        }
-
-        System.out.println("✅ Cloudinary env vars loaded!");
-
-        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", cloudName,
-                "api_key", apiKey,
-                "api_secret", apiSecret
-        ));
-    }
-
+//    public CloudinaryService() {
+//        String cloudName = System.getenv("CLOUDINARY_NAME");
+//        String apiKey = System.getenv("CLOUDINARY_API_KEY");
+//        String apiSecret = System.getenv("CLOUDINARY_API_SECRET");
+//
+//
+//        if (cloudName == null || apiKey == null || apiSecret == null) {
+//            throw new RuntimeException("❌ One or more Cloudinary env vars are missing!");
+//        }
+//
+//        System.out.println("✅ Cloudinary env vars loaded!");
+//
+//        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
+//                "cloud_name", cloudName,
+//                "api_key", apiKey,
+//                "api_secret", apiSecret
+//        ));
+//    }
+public CloudinaryService(
+        @Value("${cloudinary.name}") String cloudName,
+        @Value("${cloudinary.key}") String apiKey,
+        @Value("${cloudinary.secret}") String apiSecret
+) {
+    System.out.println("✅ Spring loaded Cloudinary config: " + cloudName);
+    this.cloudinary = new Cloudinary(ObjectUtils.asMap(
+            "cloud_name", cloudName,
+            "api_key", apiKey,
+            "api_secret", apiSecret
+    ));
+}
     public String uploadImage(MultipartFile file) throws IOException {
         Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
         return uploadResult.get("url").toString();
