d5ee239d03fc8e0781c79da72e7891e24fedafc4
danielgino <danielgino3@Gmail.com>
2025-05-06 12:36:04 +0300

    Render

--- src/main/java/org/example/apimywebsite/repository/UserRepository.java
+++ src/main/java/org/example/apimywebsite/repository/UserRepository.java
@@ -4,7 +4,6 @@ package org.example.apimywebsite.repository;
 import org.example.apimywebsite.api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,8 +11,8 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @Query("SELECT u FROM User u WHERE u.email = :email")
     User findByEmail(String email);
 
-    @Query("SELECT u FROM User u WHERE u.userName LIKE %:name%")
-    List<User> findByNameContaining(@Param("name") String name);
+    @Query("SELECT u FROM User u WHERE u.userName = :userName")
+    List<User> findByNameContaining(String name);
     @Query("SELECT u FROM User u WHERE u.name LIKE %:name%")
 
     User findByUserName(String userName);
