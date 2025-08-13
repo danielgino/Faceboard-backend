feba50344651b110e994e92bb197cb8a2e298c9b
danielgino <danielgino3@Gmail.com>
2025-08-13 09:48:44 +0300

    Some Changes

--- src/main/java/org/example/apimywebsite/ApiMyWebsiteApplication.java
+++ src/main/java/org/example/apimywebsite/ApiMyWebsiteApplication.java
@@ -21,14 +21,14 @@ public class ApiMyWebsiteApplication {
 //        System.out.println("🔍 CLOUDINARY_API_SECRET present = " + (System.getenv("CLOUDINARY_API_SECRET") != null));
 
 //LOCALHOST
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("CLOUDINARY_NAME", dotenv.get("CLOUDINARY_NAME"));
-        System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
-        System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+//        Dotenv dotenv = Dotenv.load();
+//        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+//        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+//        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+//        System.setProperty("CLOUDINARY_NAME", dotenv.get("CLOUDINARY_NAME"));
+//        System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
+//        System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
+//        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
 
         SpringApplication.run(ApiMyWebsiteApplication.class, args);
         System.out.println("✅ Application started!"); // האם אתה רואה את זה?
